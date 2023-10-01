ea8ace365a006677dd6cac63f9db43287ec54426
Daniil-Markelov <N00202705@student.iadt.ie>
2023-10-01 20:33:46 +0100

    First Commit for first page

--- Books2You/resources/views/welcome.blade.php
+++ Books2You/resources/views/welcome.blade.php
@@ -1,71 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container mx-5">
-        <div class="row justify-content-center">
-            <div>
-                <h1>Hello World</h1>
-                <h3>This is a laravel-bootstrap template</h3>
-                <div class="mt-5">
-                    <button type="button" class="btn btn-primary">Primary</button>
-                    <button type="button" class="btn btn-secondary">Secondary</button>
-                    <button type="button" class="btn btn-success">Success</button>
-                    <button type="button" class="btn btn-danger">Danger</button>
-                    <button type="button" class="btn btn-warning">Warning</button>
-                    <button type="button" class="btn btn-info">Info</button>
-                    <button type="button" class="btn btn-light">Light</button>
-                    <button type="button" class="btn btn-dark">Dark</button>
-
-                    <button type="button" class="btn btn-link">Link</button>
-                </div>
-
-                <div class="row row-cols-1 row-cols-md-3 g-4 mt-5">
-                    <div class="col">
-                        <div class="card">
-                            <img src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-social.png"
-                                class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in
-                                    to additional content. This content is a little bit longer.</p>
+<div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <section class="py-4 py-xl-5">
+                    <div class="container h-100">
+                        <div class="row h-100">
+                            <div class="col-md-10 col-xl-8 text-center d-flex d-sm-flex d-md-flex justify-content-center align-items-center mx-auto justify-content-md-start align-items-md-center justify-content-xl-center">
+                                <div>
+                                    <h2 class="text-uppercase fw-bold mb-3"><br>Curated, crafted, and delivered to you.<br><br></h2>
+                                    <p class="mb-4">Unlock Your Next Chapter: Elevate Your Reading Experience with Handpicked Books Delivered Monthly. Discover Stories, Dive into Adventures, and Cultivate Your Love for Reading, One Page at a Time.</p>
+                                    <a class="btn btn-primary fs-5 me-2 py-2 px-4" role="button" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                    <a class="btn btn-primary fs-5 me-2 py-2 px-4" role="button" href="{{ route('login') }}">{{ __('Sign Up') }}</a>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div class="col">
-                        <div class="card">
-                            <img src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-social.png"
-                                class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in
-                                    to additional content. This content is a little bit longer.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card">
-                            <img src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-social.png"
-                                class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in
-                                    to additional content.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card">
-                            <img src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-social.png"
-                                class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in
-                                    to additional content. This content is a little bit longer.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                </section>
             </div>
         </div>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
 @endsection
